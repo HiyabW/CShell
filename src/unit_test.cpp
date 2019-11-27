c5e24720d8791c3166ffb5d3d4042b7d56553997
Ruth Navarrete <rnava021@ucr.edu>
2019-11-26 17:11:52 -0800

    Modified unit_test.cpp includes to investigate unit tests

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -3,7 +3,7 @@
 
 #include "gtest/gtest.h"
 //#include "../unit_tests/ExecutableMock.hpp"
-#include "../unit_tests/Connector_test.hpp" 
+//#include "../unit_tests/Connector_test.hpp" 
 #include "../unit_tests/Executable_unit_test.hpp"
 
 int main(int argc, char **argv) {
