5da9c6b519eaf09da1fd3eea657f00cda528a17e
Ruth Navarrete <rnava021@ucr.edu>
2019-11-26 17:31:17 -0800

    All integration tests working except test_symbolic

--- unit_tests/Executable_unit_test.hpp
+++ unit_tests/Executable_unit_test.hpp
@@ -6,7 +6,7 @@
 
 TEST(ExecutableUnitTest, FailedExecutable) {
      Connector* test = new Connector();
-     std:: cout << "run: ls -4; exit" << std::endl;
+     std:: cout << "run: ls -4\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 1);
 }
@@ -14,70 +14,70 @@ TEST(ExecutableUnitTest, FailedExecutable) {
 
 TEST(ExecutableUnitTest, ExecutableLs) {
      Connector* test = new Connector();
-     std:: cout << "run: ls; exit" << std::endl;
+     std:: cout << "run: ls\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 0);
 }
 
 TEST(ExecutableUnitTest, ExecutableLsA) {
      Connector* test = new Connector();
-     std:: cout << "run: ls -a; exit" << std::endl;
+     std:: cout << "run: ls -a\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 0);
 }
 
 TEST(ExecutableUnitTest, ExecutableGit) {
      Connector* test = new Connector();
-     std:: cout << "run: git; exit" << std::endl;
+     std:: cout << "run: git\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 1);
 }
 
 TEST(ExecutableUnitTest, ExecutableGitStatus) {
      Connector* test = new Connector();
-     std:: cout << "run: git status; exit" << std::endl;
+     std:: cout << "run: git status\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 0);
 }
 
 TEST(ExecutableUnitTest, ExecutableTesteDir) {
      Connector* test = new Connector();
-     std:: cout << "run: test -e src/ ; exit" << std::endl;
+     std:: cout << "run: test -e src/\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 0);
 }
 
 TEST(ExecutableUnitTest, ExecutableTesteFile) {
      Connector* test = new Connector();
-     std:: cout << "run: test -e names.txt ; exit" << std::endl;
+     std:: cout << "run: test -e names.txt\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 0);
 }
 
 TEST(ExecutableUnitTest, ExecutableTestd) {
      Connector* test = new Connector();
-     std:: cout << "run: test -d src/ ; exit" << std::endl;
+     std:: cout << "run: test -d src/\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 0);
 }
 
 TEST(ExecutableUnitTest, ExecutableTestf) {
      Connector* test = new Connector();
-     std:: cout << "run: test -f names.txt ; exit" << std::endl;
+     std:: cout << "run: test -f names.txt\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 0);
 }
 
 TEST(ExecutableUnitTest, FailedExecutableTest) {
      Connector* test = new Connector();
-     std:: cout << "run: test -d hi/ ; exit" << std::endl;
+     std:: cout << "run: test -d hi/\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 1);
 }
 
 TEST(ExecutableUnitTest, MissingExecutableTest) {
      Connector* test = new Connector();
-     std:: cout << "run: test src/ ; exit" << std::endl;
+     std:: cout << "run: test src/\nexit" << std::endl;
      //test->parse();
      EXPECT_EQ(test->run(test), 0);
 }
