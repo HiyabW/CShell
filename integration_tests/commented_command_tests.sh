26a3a479a7dd902a53af13a790271e9688774181
Ruth Navarrete <rnava021@ucr.edu>
2019-11-27 08:56:26 -0800

    Modified integration tests to ensure proper working

--- integration_tests/commented_command_tests.sh
+++ integration_tests/commented_command_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-0<integration_tests/commented_command.txt
-:>integration_tests/commented_output.txt
-./rshell < integration_tests/commented_command.txt >> integration_tests/commented_output.txt
+0</integration_tests/commented_command.txt
+:>/integration_tests/commented_output.txt
+./rshell < /integration_tests/commented_command.txt >> /integration_tests/commented_output.txt
